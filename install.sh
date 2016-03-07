f5d748db0dcc7f86d541d88dc297e9407ce46ed8
William Volin <williamvolin@live.com>
2016-03-07 10:42:50 -0600

    update dotfiles

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 # Fix backspace issues with nvim and terminal - https://github.com/neovim/neovim/issues/2048#issuecomment-78045837
 infocmp $TERM | sed 's/kbs=^[hH]/kbs=\\177/' > $TERM.ti
 tic $TERM.ti
+# you can remove the generate file.
+
 # Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # zsh
